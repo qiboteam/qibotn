73a747f2bd36efc1aea10334f150f4d5e2b66741
Alessandro Candido <candido.ale@gmail.com>
2023-02-07 12:38:20 +0100

    Temporary solution to qiboteam/qibojit#107 in tests

--- tests/test_qasm_quimb_backend.py
+++ tests/test_qasm_quimb_backend.py
@@ -1,9 +1,12 @@
-import pytest
+import os
 
-from qibotn import qasm_quimb
+import pytest
 
 
 @pytest.mark.parametrize("nqubits", [1, 2, 5, 10])
 def test_eval(nqubits: int):
+    os.environ["QUIMB_NUM_PROCS"] = str(os.cpu_count())
+    from qibotn import qasm_quimb
+
     print(f"Testing for {nqubits} nqubits")
     result = qasm_quimb.eval_QI_qft(nqubits)
