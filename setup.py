625e3ecd95b4e2c4ac66949ed00d6f31c1275efe
Alessandro Candido <candido.ale@gmail.com>
2023-02-07 09:27:10 +0100

    Trim down setup file and deps

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 # Installation script for python
 from setuptools import setup, find_packages
-import os
-import re
+import pathlib
 
 PACKAGE = "qibotn"
 
-# load long description from README
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+HERE = pathlib.Path(__file__).parent
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="qibotn",
-    version="0.1",
+    version="0.0.1",
     description="A tensor-network translation module for quantum computing",
     author="The Qibo team",
     author_email="",
@@ -27,19 +24,14 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
     ],
     install_requires=[
-        "networkx>=2.3",
-        "opt_einsum>=3.2",
-        "autoray>=0.2.0",
-        "diskcache>=3.0",
-        "randomgen>=1.18",
         "quimb",
-        "qibo"
+        "qibo",
     ],
     extras_require={
-        "docs": ["sphinx", "sphinx_rtd_theme", "recommonmark", "sphinxcontrib-bibtex", "sphinx_markdown_tables", "nbsphinx", "IPython", "doc2dash>=2.4.1", ],
-        "tests": ["pytest", "cirq", "ply", "sklearn", "dill", "coverage", "pytest-cov"],
+        "docs": [],
+        "tests": ["pytest", "pytest-cov"],
     },
     python_requires=">=3.7.0",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
 )
