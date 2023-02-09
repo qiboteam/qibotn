339cce8207dac4d135a1c0d06d5edae8662e59e2
Alessandro Candido <candido.ale@gmail.com>
2023-02-09 12:34:35 +0100

    Start parsing gates
    Attempt code deduplication, and more declarative style at the same time
    (maybe not a great idea)

--- src/qibotn/qasm_quimb.py
+++ src/qibotn/qasm_quimb.py
@@ -1,131 +1,115 @@
 import re
 import copy
+from dataclasses import dataclass, field
+from enum import Enum
 from timeit import default_timer as timer
+from typing import Callable, Dict, List
 
 import numpy as np
-import quimb as qu
 import quimb.tensor as qtn
 
 import qibo
 from qibo.models import QFT as qibo_qft
 
+# string manipulators
 
-def get_gate_params(operation):
-    if "h " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "H")
-    elif "x " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "X")
-    elif "y " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "Y")
-    elif "z " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "Z")
-    elif "s " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "S")
-    elif "t " in operation:
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no.insert(0, "T")
-    elif "cu1" in operation:
-        lamda = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no[0:0] = ["CU1", lamda]
-    elif "cu2" in operation:
-        angles = re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0])
-        phi = float(".".join(angles[0:2]))
-        lamba = float(".".join(angles[2:]))
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no[0:0] = ["CU2", phi, lamda]
-    elif "cu3" in operation:
-        angles = re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0])
-        theta = float(".".join(angles[0:2]))
-        phi = float(".".join(angles[2:4]))
-        lamba = float(".".join(angles[4:]))
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no[0:0] = ["CU3", theta, phi, lamda]
-    elif " cx " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CX")
-    elif " cy " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CY")
-    elif " cz " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CZ")
-    elif " ccx " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CCX")
-    elif " ccy " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CCY")
-    elif " ccz " in operation:
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no.insert(0, "CCZ")
-    elif " rx " in operation:
-        theta = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no[0:0] = ["RX", theta]
-    elif "^ry " in operation:
-        theta = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no[0:0] = ["RY", theta]
-    elif "^rz " in operation:
-        theta = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no[0:0] = ["RZ", theta]
-    elif "^rzz " in operation:
-        theta = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = re.findall(r"\d+", operation.split(" ")[1])
-        qbit_no = [int(x) for x in qbit_no]
-        qbit_no[0:0] = ["RZZ", theta]
-    elif "^u1 " in operation:
-        lamda = float(
-            ".".join(re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0]))
-        )
-        qbit_no = [int(re.findall(r"\d+", operation)[0])]
-        qbit_no[0:0] = ["U1", lamda]
-    elif "^u2 " in operation:
-        angles = re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0])
-        phi = float(".".join(angles[0:2]))
-        lamba = float(".".join(angles[2:]))
-        qbit_no = int(re.findall(r"\d+", operation)[0])
-        qbit_no[0:0] = ["U2", phi, lamda]  # pylint: disable=E1137
-    elif "^u3 " in operation:
-        angles = re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", operation.split(" ")[0])
-        theta = float(".".join(angles[0:2]))
-        phi = float(".".join(angles[2:4]))
-        lamba = float(".".join(angles[4:]))
-        qbit_no = int(re.findall(r"\d+", operation)[0])
-        qbit_no[0:0] = ["U3", theta, phi, lamda]  # pylint: disable=E1137
+
+def extract_ints(string: str):
+    return re.findall(r"\d+", string)
+
+
+def extract_floats(string: str):
+    return re.findall(r"\b\d+(?:[Ee][+-]?\d+)?", string)
+
+
+def parse_float(strings: List[str]):
+    return float(".".join(strings))
+
+
+def chunk(string: str, n: int):
+    return string.split(" ")[n]
+
+
+# parameters extractors
+
+
+@dataclass
+class Parameter:
+    extractor: Callable
+    preprocess: Callable = lambda x: x
+    postprocess: Callable = lambda x: x
+
+    def extract(self, string):
+        return self.postprocess(self.extractor(self.preprocess(string)))
+
+
+class ParameterKind(Enum):
+    FirstInt = Parameter(extract_ints, postprocess=lambda x: int(x[0]))
+    IntSecond = Parameter(
+        extract_ints,
+        preprocess=lambda x: chunk(x, 1),
+        postprocess=lambda x: int(x[0]),
+    )
+    FirstFloat = Parameter(
+        extract_floats, preprocess=lambda x: chunk(x, 0), postprocess=parse_float
+    )
+
+
+ParK = ParameterKind
+
+
+# Gates
+
+
+@dataclass
+class Gate:
+    name: str
+    tag: str
+    parameters: Dict[str, ParameterKind] = field(default_factory=dict)
+
+
+class GateKind(Enum):
+    H = Gate("H", "h ", {"p0": ParK.FirstInt})
+    X = Gate("X", "x ", {"p0": ParK.FirstInt})
+    Y = Gate("Y", "y ", {"p0": ParK.FirstInt})
+    Z = Gate("Z", "z ", {"p0": ParK.FirstInt})
+    S = Gate("S", "s ", {"p0": ParK.FirstInt})
+    T = Gate("T", "t ", {"p0": ParK.FirstInt})
+    CU1 = Gate("CU1", "cu1", {"lambda": ParK.FirstFloat, "p0": ParK.FirstInt})
+    CU2 = Gate("CU2", "cu2", {"phi": ParK.FirstFloat, "lambda": ParK.FirstInt, "p0": ParK.FirstInt}})
+    CU3 = Gate("CU3", "cu3", {"theta": ParK.FirstFloat, "phi": ParK.FirstFloat, "lambda": ParK.FirstInt, "p0": ParK.FirstInt})
+    CX = Gate("CX", " cx ", {"p0": ParK.IntSecond})
+    CY = Gate("CY", " cy ", {"p0": ParK.IntSecond})
+    CZ = Gate("CZ", " cz ", {"p0": ParK.IntSecond})
+    CCX = Gate("CCX", " ccx ", {"p0": ParK.IntSecond})
+    CCY = Gate("CCY", " ccy ", {"p0": ParK.IntSecond})
+    CCZ = Gate("CCZ", " ccz ", {"p0": ParK.IntSecond})
+    RX = Gate("RX", " rx ", {"p0": ParK.FirstFloat, "theta": ParK.FirstInt})
+    RY = Gate("RY", "^ry ", {"p0": ParK.FirstFloat, "theta": ParK.FirstInt})
+    RZ = Gate("RZ", "^rz ", {"p0": ParK.FirstFloat, "theta": ParK.FirstInt})
+    RZZ = Gate("RZZ", "^rzz ", {"p0": ParK.FirstFloat, "theta": ParK.FirstInt})
+    U1 = Gate("U1", "^u1 ", {"p0": ParK.FirstFloat, "lambda": ParK.FirstInt})
+    U2 = Gate("U2", "^u2 ")
+    U3 = Gate("U3", "^u3 ")
+
+
+def gate_params(operation: str):
+    qbit_no = []
+
+    for kind in GateKind:
+        gate = kind.value
+        if gate.tag in operation:
+            qbit_no.append(gate.name)
+            parameters = [par.extract(operation) for par in gate.parameters.values()]
+            qbit_no.extend(parameters)
+            break
     else:
         assert "Unsupported gate"
 
     return qbit_no
 
 
-def get_gate_functions(qasm_str, start_idx):
+def gate_functions(qasm_str, start_idx):
     func_list = []
     result = []
     idx_inc = 0
@@ -133,7 +117,7 @@ def get_gate_functions(qasm_str, start_idx):
         if "gate " in line:
             result = re.findall("[^,\s()]+", line)
         elif result and "{" not in line and "}" not in line:
-            params = get_gate_params(line)
+            params = gate_params(line)
             func_list.append(*params)
         elif "}" in line:
             print("Returning the list")
@@ -156,7 +140,7 @@ def qasm_QFT(nqubits: int, qasm_str: str, with_swaps: bool = True, psi0=None):
         elif "swap" in command:
             break
         elif "gate" in command:  # TODO: Complete gate handling
-            gate_func, increment = get_gate_functions(qasm_str, idx)
+            gate_func, increment = gate_functions(qasm_str, idx)
             pass
         elif "barrier" in command:  # TODO: Complete barrier handling
             pass
