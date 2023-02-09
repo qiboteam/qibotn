01a0d02bc2003f512acb1adc613837aff868034c
Liwei Yang <yang0345@e.ntu.edu.sg>
2023-02-09 18:35:57 +0800

    Remove unneeded print()

--- tests/test_qasm_quimb_backend.py
+++ tests/test_qasm_quimb_backend.py
@@ -8,5 +8,4 @@ def test_eval(nqubits: int):
     os.environ["QUIMB_NUM_PROCS"] = str(os.cpu_count())
     from qibotn import qasm_quimb
 
-    print(f"Testing for {nqubits} nqubits")
     result = qasm_quimb.eval_QI_qft(nqubits)
