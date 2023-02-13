98fa7650c1bc0b2ac2f2bce87ed9af63c600a401
tankya2 <tankya2@ihpc.a-star.edu.sg>
2023-02-13 14:33:58 +0800

    Format change using Black

--- src/qibotn/__main__.py
+++ src/qibotn/__main__.py
@@ -5,39 +5,49 @@ import cupy as cp
 from qibo.models import *
 from timeit import default_timer as timer
 
-def parser():
 
+def parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--nqubits", default=10, type=int, help="Number of quibits in the circuits.")
+    parser.add_argument(
+        "--nqubits", default=10, type=int, help="Number of quibits in the circuits."
+    )
 
-    parser.add_argument("--circuit", default="qft", type=str,
-                    help="Type of circuit to use. See README for the list of "
-                         "available circuits.")
+    parser.add_argument(
+        "--circuit",
+        default="qft",
+        type=str,
+        help="Type of circuit to use. See README for the list of "
+        "available circuits.",
+    )
 
-    parser.add_argument("--precision", default='complex128', type=str,
-                    help="Numerical precision of the simulation. "
-                         "Choose between 'complex128' and 'complex64'.")
+    parser.add_argument(
+        "--precision",
+        default="complex128",
+        type=str,
+        help="Numerical precision of the simulation. "
+        "Choose between 'complex128' and 'complex64'.",
+    )
 
     return parser.parse_args()
 
-def run_bench(task, label):
 
-    start  = timer()
+def run_bench(task, label):
+    start = timer()
     result = task()
-    end    = timer()
+    end = timer()
     circuit_eval_time = end - start
     print(f"Simulation time: {label} = {circuit_eval_time}s")
-    
+
     return result
 
-def main(args: argparse.Namespace):
 
+def main(args: argparse.Namespace):
     print("Testing for %d nqubits" % (args.nqubits))
-    nqubits      = args.nqubits
+    nqubits = args.nqubits
     circuit_name = args.circuit
-    datatype     = args.precision
-    #Create qibo quibit
+    datatype = args.precision
+    # Create qibo quibit
 
     if circuit_name in ("qft", "QFT"):
         circuit = QFT(nqubits)
@@ -48,12 +58,13 @@ def main(args: argparse.Namespace):
     expression, operands = myconvertor.state_vector()
 
     result_qibo = run_bench(circuit, "Qibo")
-    sv_cutn     = run_bench(lambda:contract(expression, *operands), "cuQuantum cuTensorNet")
+    sv_cutn = run_bench(
+        lambda: contract(expression, *operands), "cuQuantum cuTensorNet"
+    )
 
-    #print(f"is sv in agreement?", cp.allclose(sv_cutn.flatten(), result_qibo.state(numpy=True)))
+    # print(f"is sv in agreement?", cp.allclose(sv_cutn.flatten(), result_qibo.state(numpy=True)))
     assert cp.allclose(sv_cutn.flatten(), result_qibo.state(numpy=True))
+
 
 if __name__ == "__main__":
     main(parser())
-
-
