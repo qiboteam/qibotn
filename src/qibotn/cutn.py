af199170717b2f694a8308c9d2192e28c82e94ed
tankya2 <tankya2@ihpc.a-star.edu.sg>
2023-04-19 16:44:53 +0800

    Updated function call

--- src/qibotn/cutn.py
+++ src/qibotn/cutn.py
@@ -5,6 +5,6 @@ from cuquantum import contract
 
 def eval(qibo_circ, datatype):
     myconvertor = QiboCircuitToEinsum(qibo_circ, dtype=datatype)
-    operands_expression = myconvertor.state_vector()
+    operands_expression = myconvertor.state_vector_operands()
     results = contract(*operands_expression)
     return results
