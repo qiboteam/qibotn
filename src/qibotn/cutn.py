88710f54f3503c28ac97c0c13de19e72d161edbd
Liwei Yang <yang0345@e.ntu.edu.sg>
2023-04-21 10:42:20 +0800

    Minor refactoring for conciseness

--- src/qibotn/cutn.py
+++ src/qibotn/cutn.py
@@ -5,6 +5,4 @@ from cuquantum import contract
 
 def eval(qibo_circ, datatype):
     myconvertor = QiboCircuitToEinsum(qibo_circ, dtype=datatype)
-    operands_expression = myconvertor.state_vector_operands()
-    results = contract(*operands_expression)
-    return results
+    return contract(*myconvertor.state_vector_operands())
