7abb82c1fb91109a8b5d5f1e28aa639276e0b875
tankya2 <tankya2@ihpc.a-star.edu.sg>
2023-08-15 16:03:39 +0800

    Change list conversion method

--- src/qibotn/MPSContractionHelper.py
+++ src/qibotn/MPSContractionHelper.py
@@ -99,7 +99,6 @@ class MPSContractionHelper:
         interleaved_inputs = []
         extra_mode = 3 * self.num_qubits + 2
         operator_modes = [None] * len(qubits) + [self.bra_modes[q][1] for q in qubits]
-        #qubits = list(qubits)
         qubits = [q for q in qubits]
         for i, o in enumerate(mps_tensors):
             interleaved_inputs.extend([o, self.bra_modes[i]])
