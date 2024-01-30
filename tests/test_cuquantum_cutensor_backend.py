93331aa12c864a4a7d53f856836efd1ecdbaec52
tankya2 <tankya2@ihpc.a-star.edu.sg>
2024-01-30 15:40:11 +0800

    Rename function name to be more descriptive [skip CI]

--- tests/test_cuquantum_cutensor_backend.py
+++ tests/test_cuquantum_cutensor_backend.py
@@ -42,7 +42,7 @@ def test_eval(nqubits: int, dtype="complex128"):
 
     # Test Cuquantum
     cutn_time, result_tn = time(
-        lambda: qibotn.cutn.eval(qibo_circ, dtype).flatten())
+        lambda: qibotn.eval.dense_vector_tn(qibo_circ, dtype).flatten())
 
     assert 1e-2 * qibo_time < cutn_time < 1e2 * qibo_time
     assert np.allclose(
@@ -78,7 +78,7 @@ def test_mps(nqubits: int, dtype="complex128"):
                  }}
 
     cutn_time, result_tn = time(
-        lambda: qibotn.eval.eval_mps(circ_qibo, gate_algo, dtype).flatten())
+        lambda: qibotn.eval.dense_vector_mps(circ_qibo, gate_algo, dtype).flatten())
 
     print(
         f"State vector difference: {abs(result_tn - result_sv_cp).max():0.3e}")
