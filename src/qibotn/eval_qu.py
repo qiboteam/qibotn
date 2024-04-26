fix for expectation calculation

src/qibotn/eval_qu.py
@@ -47,7 +47,7 @@ def dense_vector_tn_qu(qasm: str, initial_state, mps_opts, backend="numpy"):
 
 
 def expectation_qu(
-    qasm: str, pauli_string_pattern, initial_state, mps_opts, backend="numpy"
+    qasm: str, nqubits, pauli_string_pattern, initial_state, mps_opts, backend="numpy"
 ):
     """Calculates the expectation value of a given global observable as pauli
     string pattern.
@@ -65,7 +65,7 @@ def expectation_qu(
     # use cotengra package for tensor contractions
     import cotengra as ctg
 
-    nqubits = int(np.log2(len(initial_state)))
+    # nqubits = int(np.log2(len(initial_state)))
 
     if initial_state is not None:
         initial_state = init_state_tn(nqubits, initial_state)

tests/test_quimb_backend.py
@@ -126,7 +126,12 @@ def test_eval_expectation(nqubits: int, tolerance: float, is_mps: bool):
         gate_opt = None
 
     qibotn_exp = qibotn.eval_qu.expectation_qu(
-        qasm_circ, "XXXY", init_state_tn, gate_opt, backend=config.quimb.backend
+        qasm_circ,
+        nqubits,
+        "XXXY",
+        init_state_tn,
+        gate_opt,
+        backend=config.quimb.backend,
     )
 
     assert np.allclose(