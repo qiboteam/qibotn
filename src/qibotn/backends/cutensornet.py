22a55281a5959d1dcf79c181a4f8b241d7f73344
tankya2 <tankya2@ihpc.a-star.edu.sg>
2024-07-04 13:40:09 +0800

    Fix bug

--- src/qibotn/backends/cutensornet.py
+++ src/qibotn/backends/cutensornet.py
@@ -14,8 +14,9 @@ class CuTensorNet(QibotnBackend, NumpyBackend):  # pragma: no cover
 
     def __init__(self, runcard):
         super().__init__()
+        import cuquantum
         from cuquantum import cutensornet as cutn  # pylint: disable=import-error
-
+        
         if runcard is not None:
             self.MPI_enabled = runcard.get("MPI_enabled", False)
             self.NCCL_enabled = runcard.get("NCCL_enabled", False)
