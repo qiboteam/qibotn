72ba4165be437e622b983a170ec05871c8c8b80a
Matteo Robbiati <62071516+MatteoRobbiati@users.noreply.github.com>
2025-02-10 10:11:59 +0100

    Apply suggestions from code review
    Co-authored-by: BrunoLiegiBastonLiegi <45011234+BrunoLiegiBastonLiegi@users.noreply.github.com>

--- src/qibotn/result.py
+++ src/qibotn/result.py
@@ -26,10 +26,10 @@ class TensorNetworkResult:
         """Return calculated probabilities according to the given method."""
         if self.prob_type == "U":
             measured_probabilities = deepcopy(self.measured_probabilities)
-            for bitstring in self.measured_probabilities[self.prob_type]:
+            for bitstring, prob in self.measured_probabilities[self.prob_type].items():
                 measured_probabilities[self.prob_type][bitstring] = (
-                    self.measured_probabilities[self.prob_type][bitstring][1]
-                    - self.measured_probabilities[self.prob_type][bitstring][0]
+                   prob[1]
+                    - prob[0]
                 )
             probabilities = measured_probabilities[self.prob_type]
         else:
@@ -50,8 +50,7 @@ class TensorNetworkResult:
         """Return the statevector if the number of qubits is less than 30."""
         if self.nqubits < 20:
             return self.statevector
-        else:
-            raise_error(
-                NotImplementedError,
-                f"Tensor network simulation cannot be used to reconstruct statevector for >= 30 .",
-            )
+        raise_error(
+            NotImplementedError,
+            f"Tensor network simulation cannot be used to reconstruct statevector for >= 30 .",
+        )
